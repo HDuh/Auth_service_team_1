2b9712a9f108522264779a7f23ef79a3f8af3113
sergey.aleshin <serega_aleshin@mail.ru>
2022-07-29 01:02:13 +0300

    Сделан blocklist на redis

---
+++ application/core/jwt_blocklist.py
@@ -0,0 +1,19 @@
+import os
+
+import redis
+
+from application.extensions import jwt
+
+jwt_redis_blocklist = redis.StrictRedis(
+    host=os.getenv('REDIS_HOST'),
+    port=int(os.getenv('REDIS_PORT')),
+    db=0,
+    decode_responses=True
+)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blocklist(_, decrypted_token):
+    jti = decrypted_token['jti']
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
