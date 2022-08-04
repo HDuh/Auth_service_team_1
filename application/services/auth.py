3cd09c5e98ee73966f2fe2010e936f55b0b37627
hinesqui <hinesqui@gmail.com>
2022-08-05 02:30:06 +0300

    small refactoring

--- application/services/auth.py
+++ application/services/auth.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from flask import request
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from application.forms.auth_forms import ChangeDataForm
 from application.models import User, AuthHistory
 from application.models.models_enums import ActionsEnum
 
@@ -14,11 +13,11 @@ __all__ = (
 )
 
 
-def change_login(db, user: User, form: ChangeDataForm):
+def change_login(db, user: User, body: dict):
     """Логика смены логина (email)"""
 
-    if not User.query.filter_by(email=form.email.data).first():
-        user.email = form.email.data
+    if not User.query.filter_by(email=body['email']).first():
+        user.email = body['email']
         history = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.CHANGE_LOGIN)
 
         db.session.add(history)
@@ -29,10 +28,10 @@ def change_login(db, user: User, form: ChangeDataForm):
     return {'message': 'Login already exist'}, HTTPStatus.BAD_REQUEST
 
 
-def change_password(db, user: User, form: ChangeDataForm):
+def change_password(db, user: User, body: dict):
     """Логика смены пароля"""
-    if not check_password_hash(user.password, form.new_password.data):
-        user.password = generate_password_hash(form.new_password.data)
+    if not check_password_hash(user.password, body['new_password']):
+        user.password = generate_password_hash(body['new_password'])
         history = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.CHANGE_PASSWORD)
 
         db.session.add(history)
@@ -43,18 +42,18 @@ def change_password(db, user: User, form: ChangeDataForm):
     return {'message': 'Incorrect data'}, HTTPStatus.BAD_REQUEST
 
 
-def change_login_and_password(db, user: User, form: ChangeDataForm):
+def change_login_and_password(db, user: User, body: dict):
     """Логика смены логина (email) и пароля"""
 
-    if User.query.filter_by(email=form.email.data).first():
+    if User.query.filter_by(email=body['email']).first():
         return {'message': 'Login already exist'}, HTTPStatus.BAD_REQUEST
 
-    elif not check_password_hash(user.password, form.old_password.data):
+    elif not check_password_hash(user.password, body['old_password']):
         return {'message': 'Incorrect old password'}, HTTPStatus.BAD_REQUEST
 
     else:
-        user.email = form.email.data
-        user.password = generate_password_hash(form.new_password.data)
+        user.email = body['email']
+        user.password = generate_password_hash(body['new_password'])
         history = [
             AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.CHANGE_LOGIN),
             AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.CHANGE_PASSWORD),
@@ -64,5 +63,3 @@ def change_login_and_password(db, user: User, form: ChangeDataForm):
         db.session.commit()
 
         return {'message': 'Login and password change successfully'}, HTTPStatus.OK
-
-
