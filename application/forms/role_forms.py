c30e1987a82de62bfe011b9700187cd32da777ea
hinesqui <hinesqui@gmail.com>
2022-08-04 21:31:41 +0300

    resolve conficts

--- application/forms/role_forms.py
+++ application/forms/role_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, FieldList
 from wtforms.validators import DataRequired, ValidationError
 
 __all__ = (
