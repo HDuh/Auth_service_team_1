cbcd9915c0c683af0a069e8f5c51bb321afc9f34
hinesqui <hinesqui@gmail.com>
2022-08-09 03:45:07 +0300

    edit init

--- application/app.py
+++ application/app.py
@@ -14,18 +14,18 @@ def create_user(password):
     create_root(db, password)
 
 
-def init_api():
+def init_api(flask_app):
     from api.v1.auth.routes import bp_auth
     from api.v1.role.routes import bp_role
 
-    app.register_blueprint(bp_auth)
-    app.register_blueprint(bp_role)
+    flask_app.register_blueprint(bp_auth)
+    flask_app.register_blueprint(bp_role)
     swagger.registration(docs)
 
 
 def create_app(flask_app):
     db.init_app(flask_app)
-    init_api()
+    init_api(flask_app)
     db.create_all()
     from services.permissions import init_permissions, init_default_role
 
@@ -36,4 +36,5 @@ def create_app(flask_app):
 
 
 if __name__ == '__main__':
+    app = create_app(app)
     app.run(debug=True, host='0.0.0.0', port=5001)
