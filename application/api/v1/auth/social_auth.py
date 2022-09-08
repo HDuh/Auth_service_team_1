92e3693e7b7519026e5b4959c5011bf9e00733a9
hinesqui <hinesqui@gmail.com>
2022-09-08 23:11:06 +0300

    Yandex auth

--- application/api/v1/auth/social_auth.py
+++ application/api/v1/auth/social_auth.py
@@ -1,16 +1,19 @@
 from http import HTTPStatus
 
+import requests
 from flask import url_for, request
 from flask_apispec import doc, marshal_with
 from flask_restful import Resource, abort
-from werkzeug.security import generate_password_hash
 
 from application.core import PROJECT_CONFIG
 from application.extensions import providers, db
-from application.models import Provider, User, AuthHistory, Role
+from application.models import Provider, AuthHistory
 from application.models.models_enums import ActionsEnum
 from application.schemas.responses_schemas import ResponseSchema
 from application.services import get_tokens
+from application.services.auth import register_provider_user
+
+YANDEX_USER_INFO_URL = 'https://login.yandex.ru/info'
 
 
 class SocialProvider(Resource):
@@ -25,7 +28,7 @@ class SocialProvider(Resource):
         if provider_name not in providers.keys():
             abort(http_status_code=HTTPStatus.BAD_REQUEST, message=f'{provider_name} not supported')
 
-        redirect_uri = url_for('auth.socialproviderauth', _external=True, provider_name=provider_name)
+        redirect_uri = url_for(f'auth.{provider_name}providerauth', _external=True, provider_name=provider_name)
         return providers.get(provider_name).authorize_redirect(redirect_uri)
 
 
@@ -35,24 +38,21 @@ class GoogleProviderAuth(Resource):
         description='Auth through google',
         summary='User auth'
     )
-    @marshal_with(ResponseSchema, code=302, description='Server response', apply=False)
+    @marshal_with(ResponseSchema, code=200, description='Server response', apply=False)
     @marshal_with(ResponseSchema, code=400, description='Bad server response', apply=False)
     def get(self):
         token = providers.get('google').authorize_access_token()
         user_info = token['userinfo']
         provider = Provider.query.filter_by(id=int(user_info['sub'])).first()
         if not provider:
-            user = User(
+            user = register_provider_user(
                 email=user_info['email'],
-                password=generate_password_hash(user_info['sub']),
-                social_signup=True
+                uid=user_info['sub'],
+                provider='google',
+                request=request,
+                db=db,
+                role=PROJECT_CONFIG.DEFAULT_ROLES
             )
-            new_provider = Provider(id=int(user_info['sub']), user=user, provider_name='google')
-            history_signup = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.SIGNUP)
-            history_login = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.LOGIN)
-            role = Role.query.filter_by(role_name=PROJECT_CONFIG.DEFAULT_ROLES).first()
-            user.role.append(role)
-            db.session.add_all([user, new_provider, history_signup, history_login])
         else:
             user = provider.user
             history_login = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.LOGIN)
@@ -69,19 +69,28 @@ class YandexProviderAuth(Resource):
         description='Auth through yandex',
         summary='User auth'
     )
+    @marshal_with(ResponseSchema, code=200, description='Server response', apply=False)
+    @marshal_with(ResponseSchema, code=400, description='Bad server response', apply=False)
     def get(self):
         token = providers.get('yandex').authorize_access_token()
-        # user_info = token['userinfo']
-        # TODO: доп запрос для получения информации о пользователе
-        # TODO: вынести отдельно логику добавления пользователя в бд
+        headers = {'Authorization': f'OAuth {token.get("access_token")}'}
+        response = requests.get(YANDEX_USER_INFO_URL, headers=headers, timeout=30)
+        user_info = response.json()
+        provider = Provider.query.filter_by(id=int(user_info['id'])).first()
+        if not provider:
+            user = register_provider_user(
+                email=user_info['default_email'],
+                uid=user_info['id'],
+                provider='yandex',
+                request=request,
+                db=db,
+                role=PROJECT_CONFIG.DEFAULT_ROLES
+            )
+        else:
+            user = provider.user
+            history_login = AuthHistory(user=user, user_agent=request.user_agent.string, action=ActionsEnum.LOGIN)
+            db.session.add(history_login)
 
-        return {}, HTTPStatus.OK
-        # return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
-# TODO: Описать провайдеров в extensions (гугл, яндекс, мейл).
-# TODO: Проработать пайплайн авторизации:
-#   1. получить токен и юзеринфо
-#   2. проверить наличие провайдера в БД:
-#      2.1 Если провайдер есть в БД, то получить юзера. Отдать токены
-#      2.2 Если провайдера нет в БД, получить данные пользователя из токена.
-#          Создать пользователя (поставить флаг, что зареган через сторонний сервис.). При смене пароля мы не запрашиваем у пользователя old_password.
-#          Добавить провайдера в БД. Авторизовать пользователя (отдать токены).
+        db.session.commit()
+        access_token, refresh_token = get_tokens(user)
+        return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
