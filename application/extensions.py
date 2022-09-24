3d199976664a4cc29aa43d3001b52dc4a53b8e29
Nikita Dushkov <n.dushkov@yadro.com>
2022-09-24 19:05:35 +0300

    Добавил jaeger поправил гендер

--- application/extensions.py
+++ application/extensions.py
@@ -2,12 +2,15 @@ import redis
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from authlib.integrations.flask_client import OAuth
-from flask import Flask
+from flask import Flask, request
 from flask_apispec.extension import FlaskApiSpec
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+
+from jaeger_client import Config
+from flask_opentracing import FlaskTracer
 
 from core import PROJECT_CONFIG, GoogleClient, YandexClient, MailClient
 
@@ -42,3 +45,23 @@ providers = {
     'yandex': oauth.register(**YandexClient().dict()),
     'mail': oauth.register(**MailClient().dict()),
 }
+
+
+# @app.before_request
+# def before_request():
+#     request_id = request.headers.get('X-Request-Id')
+#     if not request_id:
+#         raise RuntimeError('request id is required')
+
+
+def initialize_tracer():
+    """ Tracing setup method """
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+        },
+        service_name='auth-service')
+    return config.initialize_tracer()
+
+
+flaskTracer = FlaskTracer(lambda: initialize_tracer(), True, app)
