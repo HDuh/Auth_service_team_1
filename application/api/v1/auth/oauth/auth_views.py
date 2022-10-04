9fef45a605c41012ff2dd515a1be9215cc89796b
sergey.aleshin <serega_aleshin@mail.ru>
2022-10-05 00:23:20 +0300

    Переделаны для работ с менеджером

--- application/api/v1/auth/oauth/auth_views.py
+++ application/api/v1/auth/oauth/auth_views.py
@@ -2,24 +2,29 @@ from http import HTTPStatus
 
 from flask_restful import Resource, abort
 
-from api.v1.auth.oauth.social_auth import OAuthProvider
-from extensions import providers
+from core.providers_map import ProvidersMap
+
+__all__ = (
+    'SocialAuthorize',
+    'SocialCallback',
+)
 
 
 class SocialAuthorize(Resource):
     """ Обращение к провайдеру """
+
     @staticmethod
     def get(provider_name: str):
-        if provider_name not in providers:
+        if provider_name not in ProvidersMap.__annotations__:
             abort(http_status_code=HTTPStatus.BAD_REQUEST, message=f'{provider_name} not supported')
-
-        oauth = OAuthProvider.get_provider(provider_name)
-        return oauth.authorize()
+        provider = ProvidersMap.__annotations__[provider_name]
+        return provider.manager.authorize()
 
 
 class SocialCallback(Resource):
     """ Обработка обратного вызова от провайдера """
+
     @staticmethod
     def get(provider_name: str):
-        oauth = OAuthProvider.get_provider(provider_name)
-        return oauth.callback()
+        provider = ProvidersMap.__annotations__[provider_name]
+        return provider.callback()
